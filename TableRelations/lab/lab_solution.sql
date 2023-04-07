32e89b52ad2a798855424ef806fd3620ba362848
Lachezar Ivanov <128148925+LachezarIvanovv@users.noreply.github.com>
2023-04-07 21:28:44 +0300

    Table Relations lecture exercises

---
+++ TableRelations/lab/lab_solution.sql
@@ -0,0 +1,50 @@
+#1. Mountains and Peaks
+
+CREATE TABLE `mountains`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `peaks`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `mountain_id` INT,
+    CONSTRAINT fk_peaks_mountains
+    FOREIGN KEY(`mountain_id`)
+    REFERENCES mountains(`id`)
+);
+
+#2. Trip Organization
+
+SELECT `driver_id`, `vehicle_type`, 
+CONCAT(`first_name`, ' ', `last_name`) AS 'driver_name'
+FROM `vehicles` AS v
+JOIN `campers` AS c
+ON v.`driver_id` = c.`id`;
+
+#3. SoftUni Hiking
+
+SELECT `starting_point` AS 'route_starting_point',
+		`end_point` AS 'route_ending_point',
+        `leader_id`,
+        CONCAT(`first_name`, ' ', `last_name`) AS 'leader_name'
+FROM `routes` AS r
+JOIN `campers` AS c
+ON r.`leader_id` = c.`id`;
+
+#4. Delete Mountains
+
+CREATE TABLE `mountains` (
+   `id` INT PRIMARY KEY AUTO_INCREMENT,
+   `name` VARCHAR(45)
+   );
+   
+CREATE TABLE `peaks` (
+   `id` INT PRIMARY KEY AUTO_INCREMENT,
+   `name` VARCHAR(45),
+   `mountain_id` INT,
+CONSTRAINT `fk_p_m`
+FOREIGN KEY (`mountain_id`)
+REFERENCES `mountains`(`id`)
+ON DELETE CASCADE
+);
