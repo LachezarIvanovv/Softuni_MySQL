d64ca69408ce346d1151f8ea3f1e35acf8eca2f4
Lachezar Ivanov <128148925+LachezarIvanovv@users.noreply.github.com>
2023-04-26 23:06:25 +0300

    Database Programmability lecture exercises

--- DatabaseProgrammability/exercise/exercise_solution.sql
+++ DatabaseProgrammability/exercise/exercise_solution.sql
@@ -102,3 +102,30 @@ BEGIN
         JOIN `account_holders` AS ah ON a.`account_holder_id` = ah.`id`
         WHERE a.`id` = id;
 END$$
+
+#12. Deposit Money
+
+CREATE PROCEDURE usp_deposit_money(account_id INT, money_amount DECIMAL(19, 4))
+BEGIN
+	START TRANSACTION;
+    IF (money_amount <= 0) THEN ROLLBACK;
+    ELSE
+		UPDATE `accounts` SET `balance` = `balance`+ money_amount
+        WHERE `id` = account_id;
+	COMMIT;
+    END IF;
+END$$
+
+#13. Withdraw Money
+
+CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(19, 4))
+BEGIN
+	START TRANSACTION;
+    IF(money_amount <= 0 OR 
+    (SELECT `balance` FROM `accounts` WHERE `id`= account_id) < money_amount) THEN ROLLBACK;
+    ELSE
+		UPDATE `accounts` SET `balance` = `balance` - money_amount
+        WHERE `id` = account_id;
+	COMMIT;
+    END IF;
+END$$
