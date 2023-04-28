62564c718245a606284425408fda96f3ccf5fe41
Lachezar Ivanov <128148925+LachezarIvanovv@users.noreply.github.com>
2023-04-28 21:17:51 +0300

    Database Programmability lecture exercises

--- DatabaseProgrammability/exercise/exercise_solution.sql
+++ DatabaseProgrammability/exercise/exercise_solution.sql
@@ -148,3 +148,20 @@ BEGIN
         COMMIT;
 	END IF;
 END$$
+
+#15. Log Accounts Trigger
+
+CREATE TABLE `logs`(
+	`log_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `account_id` INT NOT NULL,
+    `old_sum` DECIMAL(19, 4) NOT NULL,
+    `new_sum` DECIMAL(19, 4) NOT NULL
+);
+
+CREATE TRIGGER tr_change_balance_account
+AFTER UPDATE ON `accounts`
+FOR EACH ROW
+BEGIN
+	INSERT INTO `logs`(`account_id`, `old_sum`, `new_sum`)
+    VALUES(OLD.`id`, OLD.`balance`, NEW.`balance`);
+END$$
